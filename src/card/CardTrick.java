f45b5577ca258464028e7e92cd6af2b13f6a3927
khangvu1911 <vunguye@sheridancollege.ca>
2025-09-18 23:05:23 -0400

    Added hard-coded luckyCard to CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -21,6 +21,10 @@ public class CardTrick {
         
         Card[] magicHand = new Card[7];
         
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Clubs");
+        
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
